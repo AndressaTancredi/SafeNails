cce3dab093a5ad8747864d2f5b2d48d6a2d287b5
Andressa Tancredi <andressa.tancredi@hotmail.com>
2023-09-08 14:57:42 -0300

    add crop image

--- lib/common/image_picker_service.dart
+++ lib/common/image_picker_service.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/foundation.dart';
+import 'package:image_cropper/image_cropper.dart';
 import 'package:image_picker/image_picker.dart';
 
 class ImagePickerService {
   final _picker = ImagePicker();
+  final _cropper = ImageCropper();
 
   ImagePickerService();
+
+  Future<XFile?> cropSelectedImage(XFile file) async {
+    try {
+      final croppedFile = await _cropper.cropImage(
+        sourcePath: file.path,
+        aspectRatioPresets: [
+          CropAspectRatioPreset.square,
+        ],
+      );
+      if (croppedFile == null) {
+        throw Exception('Erro ao cortar a imagem.');
+      }
+      return XFile(croppedFile.path);
+    } catch (e) {
+      if (kDebugMode) {
+        print('Erro ao cortar a imagem: $e');
+      }
+      return null;
+    }
+  }
 
   Future<XFile?> pickImageFromGallery() async {
     try {
@@ -12,10 +34,11 @@ class ImagePickerService {
         source: ImageSource.gallery,
         maxHeight: 800,
         maxWidth: 800,
-        // imageQuality: 80,
       );
-      if (pickedFile == null) throw Exception('Nenhuma imagem selecionada');
-      return pickedFile;
+      if (pickedFile == null) {
+        throw Exception('Nenhuma imagem selecionada');
+      }
+      return cropSelectedImage(XFile(pickedFile.path));
     } catch (e) {
       if (kDebugMode) {
         print('Erro ao selecionar imagem da galeria: $e');
@@ -30,10 +53,11 @@ class ImagePickerService {
         source: ImageSource.camera,
         maxHeight: 800,
         maxWidth: 800,
-        // imageQuality: 80,
       );
-      if (pickedFile == null) throw Exception('Nenhuma imagem tirada');
-      return pickedFile;
+      if (pickedFile == null) {
+        throw Exception('Nenhuma imagem tirada');
+      }
+      return cropSelectedImage(XFile(pickedFile.path));
     } catch (e) {
       if (kDebugMode) {
         print('Erro ao tirar foto: $e');
